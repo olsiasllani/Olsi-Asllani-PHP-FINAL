288160fd8a8beabf9ec509ca0c07777240964792
olsiasllani <olsiasllani08@gmail.com>
2024-12-08 23:42:24 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Anna's Pastry</title>
+	<link rel="stylesheet" type="text/css" href="about.css">
+   
+</head>
+<body>
+	<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title>Responsive Navigation Menu</title>
+      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+   <body>
+      <?php include_once 'header.php'; ?>
+
+
+     
+       <div class="responsive-container-block bigContainer">
+  <div class="responsive-container-block Container">
+    <img class="mainImg" src="logowhite.png">
+    <div class="allText aboveText">
+      <p class="text-blk headingText">
+        
+      </p>
+      <p class="text-blk subHeadingText">
+       
+      </p>
+      <p class="text-blk description">
+       Anna's Pastry Cakes & Sweets është një destinacion i veçantë për të gjithë ata që kërkojnë ëmbëlsira të jashtëzakonshme dhe pasuri shijesh të pakrahasueshme.Ne jemi të përkushtuar për të ofruar një përvoje unike në çdo kafshatë, duke kommbinuar teknikën tradicionale me krijimtarinë moderne për të kriuar torta dhe ëmbëlsira që janë jo vetëm të shijshme,por gjithashtu vizualisht mahnitëse.
+      </p>
+      <button class="explore">
+        Explore
+      </button>
+    </div>
+  </div>
+  <div class="responsive-container-block Container bottomContainer">
+    <img class="mainImg" src="logopink.png">
+    <div class="allText bottomText">
+      <p class="text-blk headingText">
+        Our Vision
+      </p>
+      <p class="text-blk subHeadingText">
+        
+      </p>
+      <p class="text-blk description">
+        Ne synojmë të krijojmë një eksperiencë të paharrueshme për klientët tanë, duke ofruar ëmbëlsira të freskëta, të shijshme dhe me cilësi të lartë. Angazhohemi për përdorimin e përbërësve të natyrshëm dhe të freskët, duke sjellë pasionin dhe kreativitetin në çdo copë ëmbëlsirë që krijojmë. Qëllimi ynë është të sjellim ngrohtësi dhe gëzim përmes shijes së pasur, duke u siguruar që çdo vizitor të largohet me një buzëqeshje dhe dëshirë për të kthyer pas.
+      </p>
+      <button class="explore">
+        Explore
+      </button>
+    </div>
+  </div>
+</div>
+
+
+
+<?php include_once 'footer.php'; ?>
+
+
+</body>
+</html>
